f3384db471846a67d5f74e7d4d30503a793850d4
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-10-06 14:11:32 +0530

    admin home page completed

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,8 @@
+import Announcements from "@/components/Announcement";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalender from "@/components/EventCalender";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -26,10 +29,16 @@ const AdminPage = () => {
           </div>
         </div>
         {/* bottom chart */}
+        <div className="w-full h-[500px]">
+          <FinanceChart/>
+        </div>
 
       </div>
       {/* right */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      <EventCalender/>
+      <Announcements/>
+      </div>
     </div>
   );
 };
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,12 +9,12 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-  <div className="h-screen md:h-full flex" >
+  <div className="h-full md:h-full flex " >
     {/* left */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4  ">
         <Link href={'/'} className="flex items-center justify-center lg:justify-start gap-2">
             <Image src={'/logo.png'} alt="logo"  width={32} height={32}/>
-            <span className="hidden lg:block">SchoolMate</span>
+            <span className="hidden lg:block font-bold">SchoolMate</span>
         </Link>
         <Menu/>
     </div>
